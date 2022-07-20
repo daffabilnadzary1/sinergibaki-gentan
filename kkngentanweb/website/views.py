eb34b064a3f508fca5a6992649a9ce0714006713
daffabilnadzary1 <daffabilnadzary1@gmail.com>
2022-07-20 18:38:30 +0700

    pagination is now working, on the way to fix the layout

--- kkngentanweb/website/views.py
+++ kkngentanweb/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Activity
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 class Home(View):
