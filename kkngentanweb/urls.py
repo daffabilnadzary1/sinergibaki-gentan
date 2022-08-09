2d35c2533a576cb7bfe5bf0f1ee230e7f64812b0
daffabilnadzary1 <daffabilnadzary1@gmail.com>
2022-08-09 18:25:36 +0700

    trying

--- kkngentanweb/urls.py
+++ kkngentanweb/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', include('website.urls', namespace = 'website')),
     path('ckeditor/',include('ckeditor_uploader.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
